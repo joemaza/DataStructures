b455988aa2901aecfc3bca2f1e63d34ecac87c9c
joemaza <joseph.maza@gmail.com>
2018-07-24 17:43:13 -0700

    Deleting project; adding new project.

---
+++ Collections.Custom/Heap.cs
@@ -0,0 +1,11 @@
+﻿//=============================================================================
+// Copyright © 2018 Joseph S. Maza <joseph.maza@gmail.com>. All Rights Reserved.
+//=============================================================================
+using System;
+
+namespace Collections.Custom
+{
+    public class Heap
+    {
+    }
+}
